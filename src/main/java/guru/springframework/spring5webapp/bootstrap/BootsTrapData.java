c1221debb9e814cc805302495b3a58f6ae97421c
julitoi <julia.toifl01@gmail.com>
2021-04-26 15:31:42 +0200

    added Controllers and Thymeleaf template

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootsTrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootsTrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootsTrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootsTrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootsTrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -37,6 +41,19 @@ public class BootsTrapData implements CommandLineRunner {
         authorRepository.save(rod);
         bookRepository.save(noejb);
 
+        Publisher hanser = new Publisher("Hanser", "Hauptstraße 1", "Vienna", "Vienna", "1010");
+
+        noejb.setPublisher(hanser);
+        hanser.getBooks().add(noejb);
+        ddd.setPublisher(hanser);
+        hanser.getBooks().add(ddd);
+
+        publisherRepository.save(hanser);
+        bookRepository.save(ddd);
+        bookRepository.save(noejb);
+
+        System.out.println("All publishers: " + publisherRepository.findAll());
+        System.out.println("Number of books of Hanser: " + hanser.getBooks().size());
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
     }
